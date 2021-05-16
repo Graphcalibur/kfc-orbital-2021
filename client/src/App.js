02eaa6bfd40e2906bbf55df5258ceecc5477e71c
Steven Chua <stevengkc714@protonmail.com>
2021-05-16 21:11:17 +0800

    Added background change for the input box when the wrong letters are typed

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
             "\u00a0\u00a0\u00a0\u00a0cout << i << endl;",
             "}"
         ],
-        currLineNum: 1,
-        currInput: ""
+        curr_line_num: 0,
+        curr_input: "",
+        first_wrong: 0
     }
     
     /* When pressing enter, check if the text in the input
@@ -17,82 +18,100 @@ class App extends Component {
     the input and move on to the next line */
     handleSubmit = (event) => {
         if (event.key === "Enter") {
-            const textInput = document.getElementById("textInput");
-            const {code, currLineNum} = this.state;
+            const text_input = document.getElementById("textInput");
+            const {code, curr_line_num} = this.state;
             
-            if (textInput.value === code[currLineNum].trim()) {
+            if (text_input.value === code[curr_line_num].trim()) {
                 document.getElementById("textInput").value = "";
-                this.setState({currInput: ""});
-                
-                if (currLineNum < code.length - 1) {
-                    this.setState({currLineNum: currLineNum + 1});
-                }
+                this.setState({curr_input: "",
+                               first_wrong: 0,
+                               curr_line_num: Math.min(code.length - 1, curr_line_num + 1)});
             }
         }
     }
 
     /* Adds underline to the line if it's the current line */
-    underlineLine = (line, i) => {
-        return i === this.state.currLineNum
-            ? <u>{this.colorLine(line)}</u>
+    formatLine = (line, i) => {
+        return i === this.state.curr_line_num
+            ? this.colorLine(line)
             : line
     }
     
+    /* Color letters in the current line based on whether they
+    were typed correctly or wrongly */
     colorLine = (line) => {
-        const { currInput } = this.state;
-
         // Start at first non-whitespace character
-        let line_i = line.length - line.trim().length;
-        let input_i = 0;
+        const non_whitespace = line.length - line.trim().length;
 
-        let endCorrect = line_i;
-        let endWrong = line_i;
-
-        while (line_i < line.length && input_i < currInput.length) {
-            if (line.charAt(line_i) === currInput.charAt(input_i)) {
-                endCorrect++;
-            }
-            
-            endWrong++;
-            line_i++;
-            input_i++;
-        }
+        const first_wrong = non_whitespace + this.state.first_wrong;
+        const end_wrong = non_whitespace + this.state.curr_input.length;
 
         return <span>
-            <span style={{color:"#009933"}}>
-                {line.substring(0, endCorrect)}
-            </span>
-            <span style={{color:"#ff0000"}}>
-                {line.substring(endCorrect, endWrong)}
-            </span>
-            {line.substring(endWrong)}
+            {line.substring(0, non_whitespace)}
+            <u>
+                <span style={{color:"#009933"}}>
+                    {line.substring(non_whitespace, first_wrong)}
+                </span>
+                <span style={{color:"#ff0000"}}>
+                    {line.substring(first_wrong, end_wrong)}
+                </span>
+                {line.substring(end_wrong)}
+            </u>
         </span>
     }
 
+    /* Get first wrong character in input */
+    getFirstWrong = (line, curr_input) => {
+        const trimmed_line = line.trim();
+        let i = 0;
+
+        for (; i < trimmed_line.length && i < curr_input.length; i++) {
+            if (trimmed_line.charAt(i) !== curr_input.charAt(i)) {
+                break;
+            }
+        }
+
+        return i;
+    }
+
     handleInputChange = (event) => {
+        const {code, curr_line_num} = this.state;
+        const first_wrong = this.getFirstWrong(code[curr_line_num],
+                                               event.target.value);
+
         this.setState({
-            currInput: event.target.value
+            first_wrong: first_wrong,
+            curr_input: event.target.value
         });
+    }
+
+    getInputStyle = () => {
+        return this.state.first_wrong < this.state.curr_input.length
+            ? {backgroundColor: "#ff6666"}
+            : {}
     }
 
     render() {
         return (
-            <div className="mb-3">
-                {this.state.code.map((line, i) =>
-                    <label
-                        htmlFor="textInput"
-                        className="form-label code"
-                        key={line}
-                    >
-                        {this.underlineLine(line, i)}
-                    </label>)}
-                    
+            <div className="container-xl">
+                <div>
+                    {this.state.code.map((line, i) =>
+                        <label
+                            htmlFor="textInput"
+                            className="form-label code"
+                            key={line}
+                        >
+                            {this.formatLine(line, i)}
+                        </label>)}
+                </div>
+
                 <input
                     id="textInput"
                     type="text"
                     className="form-control code"
                     autoComplete="off"
                     placeholder="Start typing here..."
+                    style={this.getInputStyle()}
                     onKeyPress={this.handleSubmit}
                     onChange={(event) => this.handleInputChange(event)}
                 />
