5b87a6e6ce01aac001f6d45300ee813792199196
Steven Chua <stevengkc714@protonmail.com>
2021-05-25 12:34:01 +0800

    Added navigation bar and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,19 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import SoloTyping from "./pages/SoloTyping/SoloTyping";
 import ChooseLanguage from "./pages/ChooseLanguage/ChooseLanguage";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/solotyping/:lang?" component={SoloTyping} />
-            <Route path="/lang" component={ChooseLanguage} />
-          </Switch>
-        </div>
-      </Switch>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/solotyping/:lang?" component={SoloTyping} />
+          <Route path="/lang" component={ChooseLanguage} />
+        </Switch>
+      </div>
     );
   }
 }
