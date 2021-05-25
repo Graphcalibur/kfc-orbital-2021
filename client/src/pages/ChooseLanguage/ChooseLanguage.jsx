c86a6964c748070c077a2e674df62d1b208f24aa
Steven Chua <stevengkc714@protonmail.com>
2021-05-25 12:34:14 +0800

    Added random option to language

--- client/src/pages/ChooseLanguage/ChooseLanguage.jsx
+++ client/src/pages/ChooseLanguage/ChooseLanguage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import CodeLinkBtn from "./components/CodeLinkBtn.jsx";
 
 const ChooseLanguage = () => {
-    const langs = ["Python", "C++", "Java"];
+    const langs = ["Python", "C++", "Random"];
     return <div>
-        {langs.map((lang) => <CodeLinkBtn language={lang}/>)}
+        {langs.map((lang) => <CodeLinkBtn language={lang} key={lang}/>)}
     </div>
 }
 
--- client/src/pages/ChooseLanguage/components/CodeLinkBtn.jsx
+++ client/src/pages/ChooseLanguage/components/CodeLinkBtn.jsx
@@ -4,8 +4,9 @@ import { useHistory } from "react-router-dom";
 const CodeLinkBtn = (props) => {
     const history = useHistory();
     const lang = props.language;
+    const link = lang === "Random" ? "" : lang;
 
-    return <button className="btn me-2 btn-primary" onClick={() => history.push(`/solotyping/${lang}`)}>
+    return <button className="btn me-2 btn-primary" onClick={() => history.push(`/solotyping/${link}`)}>
         {lang}
     </button>
 }
