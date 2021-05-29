67a892208d34f679e85b9a178db970c8ce852cbc
Steven Chua <stevengkc714@protonmail.com>
2021-05-29 17:42:34 +0800

    added buttons for logging in

--- client/src/pages/ChooseLanguage/ChooseLanguage.jsx
+++ client/src/pages/ChooseLanguage/ChooseLanguage.jsx
@@ -2,10 +2,33 @@ import React from "react";
 import CodeLinkBtn from "./components/CodeLinkBtn.jsx";
 
 const ChooseLanguage = () => {
-    const langs = ["Python", "C++", "Java"];
-    return <div>
-        {langs.map((lang) => <CodeLinkBtn language={lang}/>)}
+  const langs = ["Python", "C++", "Java"];
+  const click = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "same-origin",
+      body: JSON.stringify({
+        username: "abacaba123",
+        password: "SpeedIAmSpeed",
+      }),
+    };
+
+    fetch("http://localhost:9000/api/authuser", requestOptions);
+  };
+
+  const click2 = () => {
+    fetch("http://localhost:9000/api/user/abacaba123/testauth");
+  };
+  return (
+    <div>
+      {langs.map((lang) => (
+        <CodeLinkBtn language={lang} />
+      ))}
+      <button onClick={click}>login as abacaba123</button>
+      <button onClick={click2}>check if abacaba123 is logged in</button>
     </div>
-}
+  );
+};
 
 export default ChooseLanguage;
