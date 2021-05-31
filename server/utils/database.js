d4a3232605c322b0df8f1bbaaf78ac7d45862063
Dan Baterisna <dan.alden.baterisna@gmail.com>
2021-05-31 18:08:27 +0800

    Add compatibility to remote database

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,16 +1,19 @@
 var util = require('util');
 var mysql = require('mysql2');
 
+let connection_settings = {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 20
+};
+
 // Create a connection and connects to the database.
 // Return the connection object after doing so.
 module.exports.get_connection = function () {
     
-    var connection = mysql.createConnection({
-        host: "localhost",
-        user: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: "testdb"
-    });
+    var connection = mysql.createConnection(connection_settings);
 
     connection.connect((err) => {
         if (!err) {
@@ -24,13 +27,7 @@ module.exports.get_connection = function () {
     return connection;
 };
 
-const con_pool = mysql.createPool({
-    connectionLimit: 20,
-    host: "localhost",
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: "testdb"
-});
+const con_pool = mysql.createPool(connection_settings);
 
 con_pool.query = util.promisify(con_pool.query).bind(con_pool);
 
