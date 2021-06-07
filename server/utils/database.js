0a6a027c7971bedf86393862be59fe5a49aea4cc
Steven Chua <stevengkc714@protonmail.com>
2021-06-07 15:54:21 +0800

    Changed fixed names to environmental variables

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,17 +1,16 @@
-var util = require('util');
-var mysql = require('mysql2');
+var util = require("util");
+var mysql = require("mysql2");
 
 // Create a connection pool for the server to use
 const con_pool = mysql.createPool({
-    connectionLimit: 20,
-    host: "localhost",
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: "testdb"
+  connectionLimit: 20,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
 
 // Create an awaitable function that can make queries
 con_pool.query = util.promisify(con_pool.query).bind(con_pool);
 
 module.exports.con_pool = con_pool;
-
