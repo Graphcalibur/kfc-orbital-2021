48773be415149f41ccb90e2ec02cd00bbd1848aa
Dan Baterisna <dan.alden.baterisna@gmail.com>
2021-06-11 20:25:24 +0800

    Add tests for User class

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -1,6 +1,4 @@
 const assert = require('assert');
-const mysqldump = require('mysqldump');
-
 const fs = require('fs');
 const {exec} = require('child_process');
 
@@ -13,19 +11,24 @@ module.exports = async () => {
 
     // Sort of a safety to make sure tests don't touch any running production DB
     assert.notStrictEqual(process.env.DATABASE_NAME, test_db_name,
-        "The test database name should be different from the regular database name.\n" +
-        "If you are 100% sure you want to run tests on the regular database, " +
-        "comment this assertion out and try again.");
+        "If you need to run tests on the same database as development," +
+        "remove this assertion and try again.");
 
 
     let {config: setupConfig} = require('../utils/database');
     setupConfig.database = test_db_name;
 
-    function save_testdb_to_backup() {
+    async function save_testdb_to_backup() {
         console.log("Saving current contents of " + test_db_name + " to " + testdb_backup_filename);
-        mysqldump({
-            connection: setupConfig,
-            dumpToFile: testdb_backup_filename
+        return new Promise((resolve, reject) => {
+            exec(`mysqldump -u${setupConfig.user} -p${setupConfig.password} -h${setupConfig.host} ${setupConfig.database} < ${testdb_backup_filename}`,
+                (err, stdout, stderr) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
         });
     };
 
@@ -48,7 +51,7 @@ module.exports = async () => {
         });
     };
     redirect_database_operations();
-    save_testdb_to_backup();
+    await save_testdb_to_backup();
     await load_testdb();
 
     console.log("Finished global setup");
--- server/tests/user_model.int.test.js
+++ server/tests/user_model.int.test.js
@@ -1,8 +1,40 @@
-const {con_pool} = require('../utils/database')
 const {User} = require('../models/User');
 
 test('can retrieve user by username', async () => {
     const u = await User.from_username("abacaba123");
     expect(u.username).toBe("abacaba123");
 });
+
+const test_username = "pqrstuser";
+const test_password = "weakpasswordLOL";
+
+test('can register user', async () => {
+    const u = await User.register(test_username, test_password);
+    expect(u.username).toBe(test_username);
+});
+
+test('can reject duplicate registrations', async () => {
+    expect.assertions(1);
+    try {
+        const u = await User.register(test_username, test_password);
+    } catch (e) {
+        expect(e).toBeInstanceOf(User.errors.UsernameAlreadyExistsError);
+    }
+});
+
+test('can authenticate user', async () => {
+    const u = await User.from_authentication(test_username, test_password);
+    expect(u.username).toBe(test_username);
+});
+
+test('can reject wrong authentication', async () => {
+    const wrong_password = "malingpassword";
+    expect.assertions(1);
+    try {
+        const u = await User.from_authentication(test_username, wrong_password);
+    } catch (e) {
+        expect(e).toBeInstanceOf(User.errors.IncorrectPasswordError);       
+    }
+});
+
 
