4a6a2f7bb6d51dfcbf67962e41cacf38ec5d2267
Steven Chua <stevengkc714@protonmail.com>
2021-06-19 16:24:11 +0800

    Added tests for SoloTyping

--- client/src/pages/SoloTyping/__tests__/Header.test.js
+++ client/src/pages/SoloTyping/__tests__/Header.test.js
@@ -12,21 +12,21 @@ const setup = (language, code_length, code_lines) => {
   );
 
   return {
-    wrapper: wrapper,
+    wrapper,
     text: wrapper.find(".text"),
   };
 };
 
 describe("Testing Header component in page SoloTyping", () => {
   test("Test 1", () => {
-    const { wrapper, text } = setup("Python", 80, 7);
+    const { text } = setup("Python", 80, 7);
     expect(text.text()).toEqual(
       "Language: PythonLength (Characters): 80Length (Lines): 7"
     );
   });
 
   test("Test 2", () => {
-    const { wrapper, text } = setup("C++", 1493, 46);
+    const { text } = setup("C++", 1493, 46);
     expect(text.text()).toEqual(
       "Language: C++Length (Characters): 1493Length (Lines): 46"
     );
