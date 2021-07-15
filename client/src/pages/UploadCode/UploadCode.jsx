4962ff3abccf078c171bb1e8449b2e2a3e4aa13f
Steven Chua <stevengkc714@protonmail.com>
2021-07-15 21:53:24 +0800

    Added form to upload code

--- client/src/pages/UploadCode/UploadCode.jsx
+++ client/src/pages/UploadCode/UploadCode.jsx
@@ -1,27 +1,87 @@
 import React, { Component } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Controlled as CodeMirror } from "react-codemirror2";
 
 class UploadCode extends Component {
   state = {
     language: "",
     code: "",
+    upload_state: 0,
   };
 
-  uploadCode = () => {
+  uploadCode = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
     fetch("/api/code/upload", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include",
       body: JSON.stringify({
-        language: "Python",
-        code: "print('Hello World')",
+        language: this.state.language,
+        code: this.state.code,
       }),
     }).then((res) => {
-      console.log(res);
+      if (res.status === 200) {
+        this.setState({ upload_state: 1 });
+      } else {
+        this.setState({ upload_state: res.status });
+      }
     });
   };
 
   render() {
-    return <h1 className="text">To Be Completed</h1>;
+    return (
+      <Container>
+        <h1 className="text">
+          <b>Upload Code</b>
+        </h1>
+        <Form onSubmit={this.uploadCode}>
+          <Form.Group>
+            <Form.Label className="text">Language</Form.Label>
+            <Form.Control
+              required
+              id="language"
+              value={this.state.language}
+              onChange={(event) => {
+                this.setState({ language: event.target.value });
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label className="text mt-3">Code</Form.Label>
+            <CodeMirror
+              id="code"
+              value={this.state.code}
+              options={{
+                theme: "cobalt",
+                lineNumbers: true,
+              }}
+              onBeforeChange={(editor, data, value) => {
+                this.setState({ code: value });
+              }}
+              onChange={(editor, data, value) => {}}
+            />
+          </Form.Group>
+
+          <Button variant="outline-info" className="mt-3" type="submit">
+            Upload Code
+          </Button>
+        </Form>
+
+        {this.state.upload_state === 0 ? (
+          <span></span>
+        ) : this.state.upload_state === 1 ? (
+          <p className="text mt-3"> Upload Successful!</p>
+        ) : (
+          <p className="text mt-3">
+            {" "}
+            Upload failed! Error: {this.state.upload_state}
+          </p>
+        )}
+      </Container>
+    );
   }
 }
 
